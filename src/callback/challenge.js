619bf756aecdc00f5d02446432bf662340fa0eca
VarkoMS <cmendozasoberanis@gmail.com>
2022-04-06 18:30:03 -0500

    Callback Hell, esto solo con fines demostrativos

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -2,6 +2,7 @@
 // Por eso se utiliza "XMLHttpRequest" en lugar de "Fetch", que seria lo mas adecuado.
 
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const API = 'https://rickandmortyapi.com/api/character/';
 
 function fetchData(url_api, callback) {
   let xhttp = new XMLHttpRequest();
@@ -9,7 +10,7 @@ function fetchData(url_api, callback) {
   xhttp.onreadystatechange = function (event) {
     if (xhttp.readyState === 4) {  // request finished and response is ready
       if (xhttp.status === 200) { // El servidor respondio con "OK"
-        callback(null, JSON.parse(xhttp.responseText))
+        callback(null, JSON.parse(xhttp.responseText));
       } else {
         const error = new Error('Error ' + url_api);
         return callback(error, null);
@@ -19,3 +20,20 @@ function fetchData(url_api, callback) {
 
   xhttp.send(); //Enviamos la solicitud
 }
+
+// Callback Hell XD
+// ESTO ES UNA MALA, PERO MUUUY MALA PRACTICA!!
+// Si tienes mas de 3 peticiones, NO USAR CALLBACKS, en su lugar, usar Promises.
+
+fetchData(API, function(error1, data1){
+  if (error1) return console.error(error1);
+  fetchData(API + data1.results[0].id, function(error2, data2){
+    if(error2) return console.error(error2);
+    fetchData(data2.origin.url, function(error3, data3) {
+      if(error3) return console.error(error3);
+      console.log(data1.info.count);
+      console.log(data2.name);
+      console.log(data3.dimension);
+    });
+  });
+});
