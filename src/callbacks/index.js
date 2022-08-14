fce4a7a70f533fe6dd65c6445938a459b2cb2ed4
varkoms <cmendozasoberanis@gmail.com>
2022-08-14 11:34:40 -0500

    Using callbacks

---
+++ src/callbacks/index.js
@@ -0,0 +1,19 @@
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+function calc(num1, num2, callback){
+  return callback(num1, num2);
+}
+
+console.log(calc(20, 20, sum)); // 40
+
+setTimeout(function(){
+  console.log(`Hola Javascript`);
+}, 5000);
+
+function gretting(name) {
+  console.log(`Hola ${name}`);
+}
+
+setTimeout(gretting, 5000, 'Cesar');
